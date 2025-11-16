6494928d3b671d53d1549000ddeab58df0d2119c
Sander Bongers <sander@bonge.rs>
2025-11-16 20:35:46 +0100

    Remove redundant path

--- fish/.config/fish/conf.d/fzf.fish
+++ fish/.config/fish/conf.d/fzf.fish
@@ -1,5 +1,3 @@
-fish_add_path -g (brew --prefix fzf)/bin
-
 set -gx FZF_DEFAULT_OPTS "--height 100% --reverse --multi --exact --preview-window down:60%:hidden --bind 'ctrl-/:toggle-preview+transform-preview-label:echo [ {} ]' --bind 'ctrl-z:ignore'"
 set -gx FZF_CTRL_R_OPTS "--header 'Copy: ⌃y' --bind 'ctrl-y:execute-silent(echo -n {} | pbcopy)+abort' --bind 'ctrl-r:ignore,ctrl-/:ignore'"
 
