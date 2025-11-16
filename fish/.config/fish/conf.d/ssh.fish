e9af550b3eeda58bd3889ee9627b6418721ad9eb
Sander Bongers <sander@bonge.rs>
2025-11-16 23:46:16 +0100

    Evaluate the output directly, as keychain emits valid fish commands

--- fish/.config/fish/conf.d/ssh.fish
+++ fish/.config/fish/conf.d/ssh.fish
@@ -10,8 +10,6 @@ if string match -q $os Darwin
     end
 else if string match -q $os Linux
     if command -q keychain
-        set -l keychain_env (keychain --quiet --quick --eval $ssh_key)
-        set -gx SSH_AUTH_SOCK (echo $keychain_env | grep SSH_AUTH_SOCK | cut -d= -f2)
-        set -gx SSH_AGENT_PID (echo $keychain_env | grep SSH_AGENT_PID | cut -d= -f2)
+        test -f $ssh_key; and eval (keychain --quiet --quick --eval $ssh_key)
     end
 end
