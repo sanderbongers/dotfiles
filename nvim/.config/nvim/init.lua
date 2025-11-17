5dae79999592d23048f0cbbfc4d71fbe5a015ab2
Sander Bongers <sander@bonge.rs>
2025-11-17 18:32:10 +0100

    Disable hl for now

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -62,12 +62,12 @@ vim.keymap.set({ 'n' }, '<A-l>', '<C-w>l')
 
 -- Highlight when yanking (copying) text.
 -- Try it with `yap` in normal mode. See `:h vim.hl.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  callback = function()
-    vim.hl.on_yank()
-  end,
-})
+-- vim.api.nvim_create_autocmd('TextYankPost', {
+--   desc = 'Highlight when yanking (copying) text',
+--   callback = function()
+--     vim.hl.on_yank()
+--   end,
+-- })
 
 -- [[ Create user commands ]]
 -- See `:h nvim_create_user_command()` and `:h user-commands`
