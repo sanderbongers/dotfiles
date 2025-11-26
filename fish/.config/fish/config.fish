6c3a275ee9c01fc09b547cecac7b01ba8b01fe26
Sander Bongers <sander@bonge.rs>
2025-11-26 10:39:01 +0100

    Disable 1004

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,7 @@
 set -gx EDITOR (command -q nvim && echo nvim || echo vim)
 set -gx ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX true
 
+# @fish-lsp-disable-next-line 1004
 test -f $__fish_config_dir/config.fish.local; and source $__fish_config_dir/config.fish.local
 
 # @fish-lsp-disable-next-line 1004
