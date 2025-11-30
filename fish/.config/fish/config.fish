2ab81144c5ce0d1b944bee9167da46cf32e1ced7
Sander Bongers <sander@bonge.rs>
2025-11-30 17:03:02 +0100

    If exists, add ~/.local/bin to PATH

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,8 @@
 set -gx EDITOR (command -q nvim && echo nvim || echo vim)
 set -gx ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX true
 
+test -d ~/.local/bin; and fish_add_path -g ~/.local/bin
+
 # @fish-lsp-disable-next-line 1004
 test -f $__fish_config_dir/config.fish.local; and source $__fish_config_dir/config.fish.local
 
