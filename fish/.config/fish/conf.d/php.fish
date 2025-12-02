170f7c32df0b900e46862cbcf7effcb2f2383ff5
Sander Bongers <sander@bonge.rs>
2025-12-02 16:25:31 +0100

    Fix composer/wp paths

--- fish/.config/fish/conf.d/php.fish
+++ fish/.config/fish/conf.d/php.fish
@@ -1,8 +1,8 @@
 if command -q php
-    abbr --add bc "php bin/console"
-end
+    abbr -a bc "php bin/console"
 
-if command -q brew
-    abbr --command={php7.4,php8.1,php8.2} composer "(brew --prefix composer)/bin"
-    abbr --command={php7.4,php8.1,php8.2} wp "(brew --prefix wp-cli)/bin"
+    if command -q brew
+        abbr --command={php7.4,php8.1,php8.2} composer "(brew --prefix composer)/bin/composer"
+        abbr --command={php7.4,php8.1,php8.2} wp "(brew --prefix wp-cli)/bin/wp"
+    end
 end
