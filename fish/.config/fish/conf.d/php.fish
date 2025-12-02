5d7c9a82b015a24a90434ecb28e7e9e31f50a87a
Sander Bongers <sander@bonge.rs>
2025-12-02 16:31:01 +0100

    Remove formula

--- fish/.config/fish/conf.d/php.fish
+++ fish/.config/fish/conf.d/php.fish
@@ -2,7 +2,7 @@ if command -q php
     abbr -a bc "php bin/console"
 
     if command -q brew
-        abbr --command={php7.4,php8.1,php8.2} composer "(brew --prefix composer)/bin/composer"
-        abbr --command={php7.4,php8.1,php8.2} wp "(brew --prefix wp-cli)/bin/wp"
+        abbr --command={php7.4,php8.1,php8.2} composer "(brew --prefix)/bin/composer"
+        abbr --command={php7.4,php8.1,php8.2} wp "(brew --prefix)/bin/wp"
     end
 end
