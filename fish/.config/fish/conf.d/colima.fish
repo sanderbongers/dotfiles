ed8fe67cd016d697f3c4f621194c8e8114abb2fe
Sander Bongers <sander@bonge.rs>
2026-03-09 22:05:45 +0100

    Delete colima/orbstack conf.d

--- fish/.config/fish/conf.d/colima.fish
+++
@@ -1,3 +0,0 @@
-if test -f ~/.colima/default/docker.sock
-    set -gx DOCKER_HOST unix://$HOME/.colima/default/docker.sock
-end
--- fish/.config/fish/conf.d/orbstack.fish
+++
@@ -1,5 +0,0 @@
-if test -f ~/.orbstack/run/docker.sock
-    source ~/.orbstack/shell/init2.fish 2>/dev/null
-
-    set -gx DOCKER_HOST unix://$HOME/.orbstack/run/docker.sock
-end
