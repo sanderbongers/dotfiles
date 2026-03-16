a03fd386a6118edf983b5faffa81ca29e774a15d
Sander Bongers <sander@bonge.rs>
2026-03-16 13:17:57 +0100

    Create Python venv and add to PATH

--- fish/.config/fish/conf.d/python.fish
+++ fish/.config/fish/conf.d/python.fish
@@ -1,5 +1,9 @@
 if command -q uv
     set -gx UV_PYTHON_PREFERENCE only-system
 
+    if not test -d ~/.local/venvs/tools
+        uv venv ~/.local/venvs/tools
+    end
+
     fish_add_path -g ~/.local/venvs/tools/bin
 end
