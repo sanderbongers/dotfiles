011c3009c09852586dd5e859595dee3a09c651c1
Sander Bongers <sander@bonge.rs>
2026-03-30 15:52:04 +0200

    Fix backward-word on Raspberry, and use shift-backspace for backspace-kill-bigword

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -2,4 +2,9 @@ function fish_user_key_bindings
     if command -q fzf
         fzf --fish | source
     end
+
+    bind alt-left backward-word
+    bind alt-right forward-word
+    bind alt-backspace backward-kill-word
+    bind shift-backspace backward-kill-bigword
 end
