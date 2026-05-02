95deac271676cf777ee3d8e1d8d8a637ff775af3
Sander Bongers <sander@bonge.rs>
2026-05-02 19:01:54 +0200

    Add sudo abbrevations

--- fish/.config/fish/conf.d/abbr.fish
+++ fish/.config/fish/conf.d/abbr.fish
@@ -1,3 +1,11 @@
-abbr -a d "cd ~/Downloads"
-abbr -a p "cd ~/Projects"
 abbr -a dotdot --regex '^\.\.+$' --function _multicd
+abbr -a s sudo
+abbr -a sv sudo vim
+
+if string match -q (uname) Darwin
+    abbr -a d cd "~/Downloads"
+    abbr -a p cd "~/Projects"
+else if string match -q (uname) Linux
+    abbr -a ss sudo systemctl
+    abbr -a sj sudo journalctl
+end
