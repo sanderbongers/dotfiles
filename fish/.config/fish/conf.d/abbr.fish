8763186469f5867044046ee943e3c671f8ab0298
Sander Bongers <sander@bonge.rs>
2026-05-03 14:00:08 +0200

    Remove tricky abbrevations

--- fish/.config/fish/conf.d/abbr.fish
+++ fish/.config/fish/conf.d/abbr.fish
@@ -1,11 +1,7 @@
 abbr -a dotdot --regex '^\.\.+$' --function _multicd
 abbr -a s sudo
-abbr -a sv sudo vim
 
 if string match -q (uname) Darwin
     abbr -a d cd "~/Downloads"
     abbr -a p cd "~/Projects"
-else if string match -q (uname) Linux
-    abbr -a ss sudo systemctl
-    abbr -a sj sudo journalctl
 end
