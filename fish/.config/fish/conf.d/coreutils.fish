00b29aaa7cf839d4ee344f441112ffa2e7b85d00
Sander Bongers <sander@bonge.rs>
2026-07-12 20:23:36 +0200

    Only add coreutis/findutils to PATH when interactive

--- fish/.config/fish/conf.d/coreutils.fish
+++ fish/.config/fish/conf.d/coreutils.fish
@@ -1,5 +1,7 @@
-if test -d /opt/homebrew/opt/coreutils/libexec/gnubin
-    fish_add_path -g /opt/homebrew/opt/coreutils/libexec/gnubin
-else if test -d /usr/local/opt/coreutils/libexec/gnubin
-    fish_add_path -g /usr/local/opt/coreutils/libexec/gnubin
+if status --is-interactive
+    if test -d /opt/homebrew/opt/coreutils/libexec/gnubin
+        fish_add_path -g /opt/homebrew/opt/coreutils/libexec/gnubin
+    else if test -d /usr/local/opt/coreutils/libexec/gnubin
+        fish_add_path -g /usr/local/opt/coreutils/libexec/gnubin
+    end
 end
--- fish/.config/fish/conf.d/findutils.fish
+++ fish/.config/fish/conf.d/findutils.fish
@@ -1,5 +1,7 @@
-if test -d /opt/homebrew/opt/findutils/libexec/gnubin
-    fish_add_path -g /opt/homebrew/opt/findutils/libexec/gnubin
-else if test -d /usr/local/opt/findutils/libexec/gnubin
-    fish_add_path -g /usr/local/opt/findutils/libexec/gnubin
+if status --is-interactive
+    if test -d /opt/homebrew/opt/findutils/libexec/gnubin
+        fish_add_path -g /opt/homebrew/opt/findutils/libexec/gnubin
+    else if test -d /usr/local/opt/findutils/libexec/gnubin
+        fish_add_path -g /usr/local/opt/findutils/libexec/gnubin
+    end
 end
