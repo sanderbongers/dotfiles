1c489823303b5eb6d783662c689b36196c5637ee
Sander Bongers <sander@bonge.rs>
2026-08-01 08:16:26 +0200

    Refactor passgen to use system randomness

--- fish/.config/fish/functions/passgen.fish
+++ fish/.config/fish/functions/passgen.fish
@@ -1,5 +1,13 @@
-if command -q op
-    function passgen
-        op item create --category Password --generate-password --dry-run --format json | jq -r '.fields[0].value'
+function passgen --description "Generate a random password"
+    while true
+        set -l password (LC_ALL=C tr -dc 'A-HJ-NP-Za-km-z2-9!._-' </dev/urandom | head -c 16)
+
+        string match -qr '[A-Z]' -- $password; or continue
+        string match -qr '[a-z]' -- $password; or continue
+        string match -qr '[2-9]' -- $password; or continue
+        string match -qr '[!._-]' -- $password; or continue
+
+        echo $password
+        return
     end
 end
