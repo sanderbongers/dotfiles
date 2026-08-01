f5d7318662882c7b50be02b3b7b6bb3d27c759e2
Sander Bongers <sander@bonge.rs>
2026-08-01 05:00:06 +0200

    Avoid persisting SSH agent variables in Fish

--- fish/.config/fish/conf.d/ssh.fish
+++ fish/.config/fish/conf.d/ssh.fish
@@ -10,6 +10,10 @@ if string match -q $os Darwin
     end
 else if string match -q $os Linux
     if command -q keychain
-        test -f $ssh_key; and eval (keychain --quiet --quick --eval $ssh_key)
+        if test -f $ssh_key
+            for assignment in (keychain --quiet --quick --query $ssh_key)
+                set -gx (string split -m 1 = -- $assignment)
+            end
+        end
     end
 end
