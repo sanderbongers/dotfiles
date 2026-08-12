6bae6164341ccfa3ba41b867549f90d664abbccf
Sander Bongers <sander@bonge.rs>
2026-08-12 13:59:34 +0200

    Fix brew not being added to PATH at first

--- fish/.config/fish/conf.d/01-homebrew.fish
+++ fish/.config/fish/conf.d/01-homebrew.fish
@@ -1,11 +1,13 @@
 test $__os = Darwin; or return
 
-if command -q brew
+set -l brew /opt/homebrew/bin/brew
+if test -x $brew
     # Load shell integration, cached until brew is updated.
+    # Keyed off the absolute path since brew is what puts itself on PATH.
     set -l cache $__fish_cache_dir/brew_shellenv.fish
-    if not test -f $cache; or test (command -s brew) -nt $cache
+    if not test -f $cache; or test $brew -nt $cache
         set -l tmp $cache.$fish_pid.tmp
-        brew shellenv fish >$tmp
+        $brew shellenv fish >$tmp
         and command mv -f $tmp $cache; or command rm -f $tmp
     end
     test -f $cache; and source $cache
