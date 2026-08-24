a924dfd74f2a35895b3b0f108f5954d40ab78cc3
Sander Bongers <14151411+sanderbongers@users.noreply.github.com>
2026-08-24 13:41:37 +0200

    Change shortcuts

--- stow/fish/.config/fish/conf.d/abbr.fish
+++ stow/fish/.config/fish/conf.d/abbr.fish
@@ -3,6 +3,6 @@ status is-interactive; or return
 abbr -a dotdot --regex '^\.\.+$' --function __dotdot_expand
 
 if test $__os = Darwin
-    abbr -a d cd "~/Downloads"
-    abbr -a p cd "~/Projects"
+    abbr -a dev cd "~/Developer"
+    abbr -a dl cd "~/Downloads"
 end
