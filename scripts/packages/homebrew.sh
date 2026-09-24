8b698b61e6be79969d137113a4219b0f94051b68
Sander Bongers <14151411+sanderbongers@users.noreply.github.com>
2026-09-24 20:03:53 +0200

    Fix command: use bash syntax, not fish

--- scripts/packages/homebrew.sh
+++ scripts/packages/homebrew.sh
@@ -46,7 +46,7 @@ case "$mode" in
         ;;
     upgrade)
         echo "$brewfile" | brew bundle install --file=- --upgrade
-        if command -q code; then
+        if command -v code >/dev/null; then
             code --update-extensions
         fi
         ;;
